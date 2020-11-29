662615f9321cae8e0c5895ae96b9aae0a4029766
riku-shiru <s1220226.uoa@gmail.com>
2020-11-29 19:06:44 +0900

    first commit

---
+++ src/components/ResetButton.vue
@@ -0,0 +1,26 @@
+<template>
+    <button @click="onClick">Reset</button>
+</template>
+
+<script lang="ts">
+    import { Component, Vue, Emit, Prop } from 'vue-property-decorator';
+
+    @Component
+    export default class ResetButton extends Vue {
+        private initialValue!: string;
+
+        @Prop()
+        public value!: string;
+
+        public created() {
+            this.initialValue = this.value;
+        }
+        
+        @Emit()
+        public input(value: string) {}
+
+        public onClick() {
+            this.input(this.initialValue);
+        }
+    }
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,47 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>{{greetText}}</p>
+    <p>挨拶した回数: {{count}}</p>
+    <p v-if='isRegulars'>いつもありがとうございます。</p>
+    <p>
+      <MyButton :greet="greetText" @clicked="onMyButtonClicked" class='my-button'>挨拶する</MyButton>
+    </p>
+    <p>
+      <ResetButton initialValue='Hello' v-model="greetText"></ResetButton>
+    </p> 
   </div>
 </template>
 
-<script>
+<script lang="ts">
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { Component, Vue, Watch } from 'vue-property-decorator';
+import MyButton from '@/components/MyButton.vue';
+import ResetButton from '@/components/ResetButton.vue';
 
-export default {
-  name: 'Home',
+@Component({
   components: {
-    HelloWorld
+    MyButton,
+    ResetButton,
+  },
+})
+
+export default class Home extends Vue {
+  private count: number = 0;
+  public greetText: string = 'HELL ON';
+
+  public get isRegulars(): boolean {
+    return this.count >= 5
   }
+
+  @Watch('count')
+  public countChanged(){
+    if(this.count === 5) alert('you became a regulars');
+  }
+
+  public onMyButtonClicked(count: number) {
+    this.count = count;
+    this.greetText = 'こんにちは';
+  } 
 }
+
 </script>
